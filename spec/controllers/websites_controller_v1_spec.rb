2b1793ccc7b8628a6480badc8c3d1d67642fdc2b
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-18 08:53:43 -0300

    12° commit - tdd controller: Consegue listar um website especifico e retornar status 200? RED

--- spec/controllers/websites_controller_v1_spec.rb
+++ spec/controllers/websites_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::WebsitesController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/websites/id' do
+    it 'Consegue listar um website especifico e retornar status 200?' do
+      get :show, params: {id: @website.id}
+      expect(response.body).to include_json(id: @website.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end

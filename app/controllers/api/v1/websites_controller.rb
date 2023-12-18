f25a6b6efee64ae3ac9be2e774fe6c6b3f6e1910
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-18 09:17:07 -0300

    19° commit - tdd controller: Consegue excluir um website e retornar status 204? GREEN

--- app/controllers/api/v1/websites_controller.rb
+++ app/controllers/api/v1/websites_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::WebsitesController < ApplicationController
 
-  before_action :set_website, only: %i[show update] # show update destroy
+  before_action :set_website, only: %i[show update destroy] # show update destroy
 
   def index
     @websites = Website.all 
@@ -26,6 +26,10 @@ class Api::V1::WebsitesController < ApplicationController
     else
       render json: @website.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @website.destroy!
   end
 
 private 
